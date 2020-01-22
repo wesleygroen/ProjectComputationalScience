2f3c09fcd1297ceea5f6d4ce9ff9d24581c22799
mgonggrijp <59604036+mgonggrijp@users.noreply.github.com>
2020-01-22 15:15:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 flow_counter = 0
 car_counter = 0
 plot_data = []
- 
+Vmax1 = 6
+Vmax2 = 5
+Vm_vary = 0
+
+# Table to define the speed, distance becomes an index into
+# this list to get the corresponding speed update response.
+# This is used for the aggresive driver scenario.
+speed_table1 = [0, 3, 5, Vmax1, Vmax1]
+
+speed_table2 = [0, 3, 4, Vmax2, Vmax2]
