33ece11dd4cca09fa613a4182e086e5fb72a4194
wesleygroen <wesleygroen.wg@gmail.com>
2020-01-24 15:16:09 +0100

    pep8

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 flow_counter = 0
 car_counter = 0
 plot_data = []
-Vmax1=6
-Vmax2=5
+Vmax1 = 6
+Vmax2 = 5
 
 
 # Vm_vary is used to seperate the slow and fast driver classes,
-# Vm_vary will be an integer number, such that the portion of 
+# Vm_vary will be an integer number, such that the portion of
 # slow drivers is equal to 1 / Vm_vary
 
-# 	We use it as a modulo operator, 
-# using i % (modulo) Vm_vary == 0 to check if the i-th driver label is 
-# a multiple of Vm_vary, and making these drivers have the alternative, lower Vmax.
+#     We use it as a modulo operator,
+# using i % (modulo) Vm_vary == 0 to check if the i-th driver label is
+# a multiple of Vm_vary, and making these drivers have the alternative,
+# lower Vmax.
 Vm_vary = 2
 
 # Table to define the speed, distance becomes an index into
